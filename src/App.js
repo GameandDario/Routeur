6989c6f65093f163636323eb6d88457f44f3a69e
Urth <dragondor@protonmail.com>
2019-04-11 23:13:10 +0200

    navlink to components History Contact Home added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch, NavLink } from 'react-router-dom';
+import Home from './Components/Home';
+import Contact from './Components/Contact';
+import History from './Components/History';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <NavLink exact to="/"> Accueil </NavLink>
+        <NavLink to="/page-de-contact" activeClassName="selected"> Contact </NavLink>
+        <NavLink to="/notre-histoire" activeStyle={{
+          fontWeight: "bold",color: "red"}}> Histoire </NavLink>
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/page-de-contact" component={Contact} />
+          <Route path="/notre-histoire" component={History} />
+        </Switch>
       </div>
     );
   }
